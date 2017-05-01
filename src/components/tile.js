b6d757ba3adb573c94d132d73aa33f4c5338dca2
Ayc0 <benjamin.koltes@student.ecp.fr>
2017-05-01 16:57:58 +0200

    Feature(Color): Change color

--- src/components/tile.js
+++ src/components/tile.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { styled } from 'styletron-react';
+import Fa from 'react-fontawesome';
 
 import PropTypes from 'prop-types';
 
@@ -9,11 +10,13 @@ const checkClickable = value => value === null || value === -2 || value === -3;
 const StyledTile = styled('div', props => ({
   height: size,
   width: size,
-  backgroundColor: checkClickable(props.value) ? 'grey' : 'white',
-  border: '1px solid black',
-  color: 'black',
+  // backgroundColor: checkClickable(props.value) ? '#ffe4af' : '#fff2d8',
+  backgroundColor: checkClickable(props.value) ? '#e2d866' : '#e8e1a7', // e6de7d
+  border: '1px solid #bcaa14',
+  color: checkClickable(props.value) ? '#7c7115' : '#9b8d1a',
   padding: 0,
   margin: 0,
+  textAlign: 'center',
 }));
 
 const Tile = props => (
@@ -23,9 +26,9 @@ const Tile = props => (
     value={props.value}
   >
     {props.value > 0 ? props.value : null}
-    {props.value === -1 ? 'B' : null}
-    {props.value === -2 ? 'F' : null}
-    {props.value === -3 ? '?' : null}
+    {props.value === -1 ? <Fa name="bomb" /> : null}
+    {props.value === -2 ? <Fa name="flag" /> : null}
+    {props.value === -3 ? <Fa name="question" /> : null}
   </StyledTile>
 );
 
